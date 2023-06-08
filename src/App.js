7f74932d37b1c5ceb75fa9be5548129b485f7041
WilhelmK109 <kamulungaw@gmail.com>
2023-06-08 21:21:19 +0200

    Add react route to render components on the UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import BookList from './components/BookList';
+import Navigation from './components/Navigation';
+import CategoriesPage from './components/Categories';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
