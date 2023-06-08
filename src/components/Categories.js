61fb7915009daab5c96aaf1431af42139f1ca40f
WilhelmK109 <kamulungaw@gmail.com>
2023-06-08 20:56:55 +0200

    Add CategoriesPage component

---
+++ src/components/Categories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function CategoriesPage() {
+  return (
+    <div>
+      <h1>Categories</h1>
+      <button type="button">Check Status</button>
+    </div>
+  );
+}
