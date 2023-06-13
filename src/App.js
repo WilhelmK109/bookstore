21f26c0e0a8293a75dd4cb22116945c816cb87ea
WilhelmK109 <kamulungaw@gmail.com>
2023-06-13 13:19:37 +0200

    Add Provider component to wrap the entire application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./redux/store";
 
-import BookList from './components/BookList';
-import Navigation from './components/Navigation';
-import CategoriesPage from './components/Categories';
+import BookList from "./components/BookList";
+import Navigation from "./components/Navigation";
+import CategoriesPage from "./components/Categories";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<BookList />} />
-          <Route path="/categories" element={<CategoriesPage />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<BookList />} />
+            <Route path="/categories" element={<CategoriesPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Provider>
   );
 }
 
