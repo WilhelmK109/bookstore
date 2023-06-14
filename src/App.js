c0b3057d134f9232037421b24ae5c3a6a21fb6a2
WilhelmK109 <kamulungaw@gmail.com>
2023-06-14 12:33:09 +0200

    Remove Provider component from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import { Provider } from 'react-redux';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import store from './redux/store';
+import { Provider } from "react-redux";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import store from "./redux/store";
 
-import BookList from './components/BookList';
-import Navigation from './components/Navigation';
-import CategoriesPage from './components/Categories';
+import BookList from "./components/BookList";
+import Navigation from "./components/Navigation";
+import CategoriesPage from "./components/Categories";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <BrowserRouter>
-          <Navigation />
-          <Routes>
-            <Route path="/" element={<BookList />} />
-            <Route path="/categories" element={<CategoriesPage />} />
-          </Routes>
-        </BrowserRouter>
-      </div>
-    </Provider>
+    <div className="App">
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/categories" element={<CategoriesPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
