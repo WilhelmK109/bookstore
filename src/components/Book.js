4584e1c0d33b2c9f542dfc9c2d1e8f8296cbe8b6
WilhelmK109 <kamulungaw@gmail.com>
2023-06-15 17:01:54 +0200

    Refactor remove book feature to persist changes in the server

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,24 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import RemoveBookButton from './RemoveBookButton';
+import { useDispatch } from 'react-redux';
+import { removeBook, deleteBook } from '../redux/books/booksSlice';
 
-function Book({ book }) {
+export default function Book({ book }) {
+  const dispatch = useDispatch();
+  const handleRemove = (bookId) => {
+    dispatch(deleteBook(bookId))
+      .then(dispatch(removeBook(bookId)));
+  };
+
   return (
     <li>
       <div className="book-info">
-        <p>{book.title}</p>
-        <span>{book.author}</span>
+        <h2 className="book-title">{book.title}</h2>
+        <span className="book-author">{book.author}</span>
+        <div className="book-buttons">
+          <button type="button">Comment</button>
+          <button type="button" onClick={() => handleRemove(book.item_id)}>Remove</button>
+          <button type="button">Edit</button>
+        </div>
       </div>
-      <RemoveBookButton bookId={book.id} />
     </li>
   );
 }
 
 Book.propTypes = {
   book: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    author: PropTypes.string.isRequired,
+    item_id: PropTypes.string,
+    title: PropTypes.string,
+    author: PropTypes.string,
   }).isRequired,
 };
-export default Book;
