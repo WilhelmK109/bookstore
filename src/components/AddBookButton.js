63ea988bf98c4f160e7da7f2bc12993f36c2cf88
WilhelmK109 <kamulungaw@gmail.com>
2023-06-15 16:27:13 +0200

    Refactor add book features to persist changes in the server

--- src/components/AddBookButton.js
+++ src/components/AddBookButton.js
@@ -1,26 +1,39 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import propTypes from 'prop-types';
-import { addBook } from '../redux/books/booksSlice';
+import { nanoid } from 'nanoid';
+import { addBook, createBook } from '../redux/books/booksSlice';
 
-export default function AddBookButton({ title, author }) {
+export default function AddBookButton() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
 
   const handleAddBook = (event) => {
     event.preventDefault();
-    const book = {
-      id: Math.random().toString(36).substring(7),
-      title,
-      author,
-    };
-    dispatch(addBook(book));
+    if (title && author) {
+      const book = {
+        item_id: nanoid(),
+        title,
+        author,
+        category: null,
+      };
+      dispatch(createBook(book))
+        .then(() => {
+          dispatch(addBook(book));
+          setTitle('');
+          setAuthor('');
+        });
+    }
   };
 
   return (
-    <button type="submit" onClick={handleAddBook}>ADD BOOK</button>
+    <div>
+      <h2>Add New Book</h2>
+      <form onSubmit={handleAddBook}>
+        <input className="input-title" type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Book title" />
+        <input className="input-author" type="text" value={author} onChange={(e) => setAuthor(e.target.value)} placeholder="Author" />
+        <button className="add-button" type="submit">ADD BOOK</button>
+      </form>
+    </div>
   );
 }
-
-AddBookButton.propTypes = {
-  title: propTypes.string.isRequired,
-  author: propTypes.string.isRequired,
-};
